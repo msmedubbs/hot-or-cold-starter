03e8f39f0b114ec9004847b2eb95bff94f95d54c
msmedubbs <MarieW@Maries-MacBook-Air.local>
2014-04-03 16:48:48 -0700

    take guess input
    input guess by enter or button
    checks if is between 1 and 100

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
-
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	$(".what").click(function(){
+		$(".overlay").fadeIn(1000);
 
-  	});
+	});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
 
+	$("#guessButton").click(function(){
+    var newGuess = $("#userGuess").val();
+        if (newGuess >= 1 && newGuess <= 100){
+            alert("between");
+        }
+
+        else{
+			alert("Type an item in before you add it.");
+        }
+    });
 });
-
-
