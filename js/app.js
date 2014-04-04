5f32b851aafc6393a4a9ec2be53bdf89df3a652c
msmedubbs <MarieW@Maries-MacBook-Air.local>
2014-04-03 18:59:43 -0700

    attempts to give user feedback
    guess counter not working
    feedback text is blinking
    issues somewhere

--- js/app.js
+++ js/app.js
@@ -1,24 +1,67 @@
-$(document).ready(function(){
-	
+var theAnswer = 10;
+var guessCounter = 0;
+
+var guessCompare = function (latestGuess) {
+	var Distance = Math.abs(theAnswer-latestGuess);
+
+	if (latestGuess === theAnswer) {
+		$("#feedback").text("You guessed my number!");
+	}
+	else if (Distance >= 50) {
+		$("#feedback").text("Ice cold");
+	}
+	else if (Distance >= 30 && Distance < 50) {
+		$("#feedback").text("Cold");
+	}
+	else if (Distance >= 20 && Distance < 30) {
+		$("#feedback").text("Warm");
+	}
+	else if (Distance >= 10 && Distance < 20) {
+		$("#feedback").text("Hot");
+	}
+	else {
+		$("#feedback").text("Very Hot");
+	}
+};
+
+var newGame = function () {
+	guessCounter = 0;
+	// generate new theAnswer
+};
+
+$(document).ready(function () {
+
 	/*--- Display information modal box ---*/
-	$(".what").click(function(){
-		$(".overlay").fadeIn(1000);
+	$(".what").click(function () {
+	$(".overlay").fadeIn(1000);
 
 	});
 
 	/*--- Hide information modal box ---*/
-	$("a.close").click(function(){
+	$("a.close").click(function () {
 		$(".overlay").fadeOut(1000);
 	});
 
-	$("#guessButton").click(function(){
-    var newGuess = $("#userGuess").val();
-        if (newGuess >= 1 && newGuess <= 100){
-            alert("between");
-        }
+	/*--- Watch for New Game click ---*/
+	$(".new").click(function () {
+		newGame();
+	});
 
+	/*--- Watch for Guess click ---*/
+	$("#guessButton").click(function () {
+    var my_entry = $("#userGuess").val();
+
+        if (my_entry >= 1 && my_entry <= 100){
+           guessCounter += 1;
+           console.log(guessCounter);
+           guessCompare(my_entry);
+           //$("#count").text();
+        }
         else{
-			alert("Type an item in before you add it.");
+			$("#feedback").text("Enter a guess from 1 to 100");
         }
     });
+
 });
+
+
